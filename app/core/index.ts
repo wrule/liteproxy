c60c16bd2991060d7ee9383db7ed25270bf78afd
jimao <shihao.gu@perfma.com>
2024-08-19 17:26:31 +0800

    HttpProxyHub

--- app/core/index.ts
+++ app/core/index.ts
@@ -83,6 +83,17 @@ class HttpProxy {
   }
 }
 
+export
+class HttpProxyHub {
+  public constructor() { }
+
+  private store = new Map<number, HttpProxy>();
+
+  public Add(port: number, config: ProxyConfig, name = '') {
+
+  }
+}
+
 export default
 async function main() {
   console.log('你好，世界');
