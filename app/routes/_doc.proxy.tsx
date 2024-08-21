e001a692eb04e800b99a19a81debfd93a1c3eb81
jimao <shihao.gu@perfma.com>
2024-08-21 16:41:45 +0800

    简单表格

--- app/routes/_doc.proxy.tsx
+++ app/routes/_doc.proxy.tsx
@@ -6,5 +6,33 @@ export const meta: MetaFunction = () => {
 
 export default
 function DocProxy() {
-  return <div>代理</div>;
+  return <div className="p-4">
+    <div></div>
+    <div>
+      <table className="w-full bg-white border-t border-x text-left">
+        <thead>
+          <tr className="border-b">
+            <th className="px-2 py-1">端口</th>
+            <th className="px-2 py-1">名称</th>
+            <th className="px-2 py-1">状态</th>
+            <th className="px-2 py-1">操作</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="border-b">
+            <td className="px-2 py-1">8080</td>
+            <td className="px-2 py-1">默认</td>
+            <td className="px-2 py-1">已启用</td>
+            <td className="px-2 py-1"></td>
+          </tr>
+          <tr className="border-b">
+            <td className="px-2 py-1">8081</td>
+            <td className="px-2 py-1">默认</td>
+            <td className="px-2 py-1">已启用</td>
+            <td className="px-2 py-1"></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>;
 }
