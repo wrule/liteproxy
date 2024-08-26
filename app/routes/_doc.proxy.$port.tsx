d15df937896479660a2e99d02b051e8c844dfd65
jimao <shihao.gu@perfma.com>
2024-08-26 15:10:21 +0800

    提交代码

--- app/routes/_doc.proxy.$port.tsx
+++ app/routes/_doc.proxy.$port.tsx
@@ -1,5 +1,5 @@
 import { LoaderFunctionArgs } from "@remix-run/node";
-import { json, MetaFunction } from "@remix-run/react";
+import { json, MetaFunction, useLoaderData } from "@remix-run/react";
 import { Button, Col, Form, Input, InputNumber, Row, Space, Switch } from "antd";
 import hub from "~/core";
 
@@ -8,41 +8,43 @@ export const meta: MetaFunction = () => {
 };
 
 export async function loader({ params }: LoaderFunctionArgs) {
-  let result = { };
-  const port = params.port;
-  if (port && port.toLowerCase() !== 'new') {
-  }
-  return json(result);
+  const port = (params.port as string).toLowerCase();
+  if (port !== 'create') return json(await hub.Find(Number(port)));
+  return json(null);
 }
 
 export default
 function DocProxyDetail() {
+  const config = useLoaderData<typeof loader>();
+  const [form] = Form.useForm();
+
   return <div className="p-4">
     <Form
+      form={form}
       layout="vertical">
       <Row gutter={15}>
         <Col span={6}>
-          <Form.Item label="端口">
+          <Form.Item label="端口" name="port" initialValue={config?.port}>
             <InputNumber
-              readOnly
+              readOnly={!!config?.port}
               className="w-full"
               placeholder="请输入端口"
             />
           </Form.Item>
         </Col>
         <Col span={18}>
-          <Form.Item label="启用">
+          <Form.Item label="启用" name="enabled" initialValue={config?.enabled}>
             <Switch />
           </Form.Item>
         </Col>
       </Row>
-      <Form.Item label="名称">
+      <Form.Item label="名称" name="name" initialValue={config?.name}>
         <Input
           className="w-full"
           placeholder="请输入名称"
         />
       </Form.Item>
-      <Form.Item label="配置">
+      <Form.Item label="配置" name="configCode" initialValue={config?.configCode}>
         <Input.TextArea
           placeholder="请输入配置代码"
           autoSize={{ minRows: 12 }}
