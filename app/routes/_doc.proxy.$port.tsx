ec98bfb6dbf55e85cf0c55e89dd331765d02ba1b
jimao <shihao.gu@perfma.com>
2024-08-26 15:45:42 +0800

    不知道为什么

--- app/routes/_doc.proxy.$port.tsx
+++ app/routes/_doc.proxy.$port.tsx
@@ -20,12 +20,13 @@ function DocProxyDetail() {
 
   return <div className="p-4">
     <Form
-      form={form}
-      layout="vertical">
-      <Row gutter={15}>
+      method="POST"
+      action="/api/proxy">
+      {/* <Row gutter={15}>
         <Col span={6}>
           <Form.Item label="端口" name="port" initialValue={config?.port}>
             <InputNumber
+              name="port"
               readOnly={!!config?.port}
               className="w-full"
               placeholder="请输入端口"
@@ -40,20 +41,22 @@ function DocProxyDetail() {
       </Row>
       <Form.Item label="名称" name="name" initialValue={config?.name}>
         <Input
+          name="name"
           className="w-full"
           placeholder="请输入名称"
         />
       </Form.Item>
       <Form.Item label="配置" name="configCode" initialValue={config?.configCode}>
         <Input.TextArea
+          name="configCode"
           placeholder="请输入配置代码"
           autoSize={{ minRows: 12 }}
         />
-      </Form.Item>
+      </Form.Item> */}
       <Form.Item>
         <div className="w-full flex justify-between">
           <span></span>
-          <Button type="primary">保存</Button>
+          <Button type="primary" htmlType="submit">保存</Button>
         </div>
       </Form.Item>
     </Form>
